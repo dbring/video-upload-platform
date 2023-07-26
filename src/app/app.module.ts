80bb6de08e565a725690c6ea264c13f38d0c1351
dbring <dbring@gmail.com>
2023-07-26 14:15:16 -0700

    Implemented user registration auth with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyDlyeo3Tj_S1jFTFgNF8Ge64s-ut9F3sCs',
+    authDomain: 'angular-video-platform-d7fb9.firebaseapp.com',
+    projectId: 'angular-video-platform-d7fb9',
+    storageBucket: 'angular-video-platform-d7fb9.appspot.com',
+    messagingSenderId: '953417065058',
+    appId: '1:953417065058:web:b73294b8c15923d1d8273b',
+  },
+};
